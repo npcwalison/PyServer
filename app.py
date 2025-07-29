ead186206b146c48481c2fa1bbb5384bc2a423c5
npcwalison <gwalisonls@gmail.com>
2025-07-29 15:25:43 -0300

    create template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from database.model import create_user_table
 from waitress import serve
 
@@ -12,7 +12,7 @@ data = ['teste', 'ola']
 # Vizualizar
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify(data), 200
+    return render_template('index.html')
 
 # Adicionar Valores
 @app.route('/register', methods=['GET'])
@@ -22,4 +22,5 @@ def register():
 # Indentifica se o arquivo atual esta sendo executado diretamente
 if __name__ == "__main__":
     create_user_table() # Verifica e cria a base de dados.
-    serve(app, host="0.0.0.0", port=5000) # Inicia o servidor indicando o IP e PORTA
+    # serve(app, host="0.0.0.0", port=5000) # Inicia o servidor indicando o IP e PORTA
+    app.run(debug=True, host="0.0.0.0", port=5000) # Inicia o servidor em modo de teste
--- database/model.py
+++ database/model.py
@@ -10,17 +10,12 @@ def create_user_table():
         try:
             cur = conn.cursor()
             cur.execute("""
+                -- CRIA SETOR COMO MESTRE DE TUDO
                 CREATE TABLE IF NOT EXISTS gti_sectors (
                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                    sc_name VARCHAR(100),
+                    sc_name VARCHAR(100)
                 );
-                CREATE TABLE IF NOT EXISTS gti_users (
-                    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                    name VARCHAR(100),
-                    sr_email VARCHAR(100),
-                    sr_sector VARCHAR(50),
-                    sector_id_sector INT --FK
-                );
+                -- CRIA MAQUINAS PRESENTES NO SETOR
                 CREATE TABLE IF NOT EXISTS gti_machines (
                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                     mh_name VARCHAR(100),
@@ -28,25 +23,51 @@ def create_user_table():
                     mh_motherboard VARCHAR(100),
                     mh_processor VARCHAR(100),
                     mh_memory_ram VARCHAR(100),
-                    mh_storege VARCHAR(100),
-                    sector_id_sector VARCHAR(50),
+                    mh_storage VARCHAR(100),
+                    sector_id_sector UUID, --FK
+                    CONSTRAINT fk_machines_sector FOREIGN KEY (sector_id_sector) REFERENCES gti_sectors(id)
                 );
+                -- CRIA USUARIOS E OS ATIBUE AOS SETORES E MAQUINAS
+                CREATE TABLE IF NOT EXISTS gti_users (
+                    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                    name VARCHAR(100),
+                    sr_email VARCHAR(100),
+                    sr_sector VARCHAR(50),
+                    sector_id_sector UUID, --FK
+                    CONSTRAINT fk_users_sector FOREIGN KEY (sector_id_sector) REFERENCES gti_sectors(id)
+                );
+                -- CRIA EVENTOS QUE PODEM SER RELACIONADOS A TUDO
                 CREATE TABLE IF NOT EXISTS gti_events (
                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                    vt_name VARCHAR(100),
-                    vt_sector VARCHAR(50),
-                    vt_wage NUMERIC(15, 4)
+                    ev_name VARCHAR(100),
+                    ev_sector VARCHAR(50),
+                    ev_description VARCHAR(8000),
+                    ev_date_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    ev_date_start TIMESTAMP NOT NULL,
+                    ev_date_end TIMESTAMP DEFAULT NULL,
+                    user_id_user UUID, --FK
+                    sector_id_sector UUID, --FK
+                    machines_id_machines UUID, --FK
+                    CONSTRAINT fk_events_user FOREIGN KEY (user_id_user) REFERENCES gti_users(id),
+                    CONSTRAINT fk_events_sector FOREIGN KEY (sector_id_sector) REFERENCES gti_sectors(id),
+                    CONSTRAINT fk_events_machines FOREIGN KEY (machines_id_machines) REFERENCES gti_machines(id)
                 );
+                -- GERA REGISTRO DE LOGIN DE USUARIOS
                 CREATE TABLE IF NOT EXISTS users_has_machines (
-                    user_id_user INT --FK
-                    machines_id_machines INT --FK
-                )
+                    user_id_user UUID, --FK
+                    machines_id_machines UUID, --FK
+                    mc_date_start TIMESTAMP NOT NULL,
+                    mc_date_end TIMESTAMP DEFAULT NULL,
+                    PRIMARY KEY (user_id_user, machines_id_machines, mc_date_start),
+                    CONSTRAINT fk_user_machines_user FOREIGN KEY (user_id_user) REFERENCES gti_sectors(id),
+                    CONSTRAINT fk_user_machines_machines FOREIGN KEY (machines_id_machines) REFERENCES gti_machines(id)
+                );
             """)
 
             conn.commit()
-            print("Tabela 'usuarios' criada com sucesso!")
+            print("Modelo de dados criado com sucesso!")
         except psycopg2.Error as e:
-            print(f"Erro ao criar tabela: {e}")
+            print(f"Erro ao criar modelo de dados: {e}")
         finally:
             if conn:
                 conn.close()
